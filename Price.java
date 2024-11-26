c48a462657f95ce0a394c04d3da027435c77f7ba
Leonardo Paes <lpaes@minha.fag.edu.br>
2024-11-25 23:16:17 -0300

    Commit 15

--- Price.java
+++ Price.java
@@ -5,7 +5,7 @@ public abstract class Price {
 
     public abstract double getCharge(int daysRented); // Método abstrato
 
-    public int getFrequentRenterPoints(int daysRented) { // Método movido e extraído
-        return 1; // Valor padrão
+    public int getFrequentRenterPoints(int daysRented) {
+        return 1; // Valor padrão para a maioria dos casos
     }
 }
