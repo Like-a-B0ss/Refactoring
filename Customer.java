14ed6678fd8948fe0f2ab0a5ac87175157436f18
Leonardo Paes <lpaes@minha.fag.edu.br>
2024-11-25 22:50:17 -0300

    Commit 4

--- Customer.java
+++ Customer.java
@@ -1,64 +1,46 @@
 package Refactoring;
+
 import java.util.Enumeration;
 import java.util.Vector;
 
 public class Customer {
-   private String _name;
-   private Vector _rentals = new Vector();
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<>();
 
-   public Customer(String name) {
-      _name = name;
-   }
+    public Customer(String name) {
+        _name = name;
+    }
 
-   public void addRental(Rental arg) {
-      _rentals.addElement(arg);
-   }
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
 
-   public String getName() {
-      return _name;
-   }
+    public String getName() {
+        return _name;
+    }
 
-   public String statement() {
-      double totalAmount = 0;
-      int frequentRenterPoints = 0;
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-         double thisAmount = amountFor(each); // Chamada ao método extraído
+    public String statement() {
+        double totalAmount = 0;
+        int frequentRenterPoints = 0;
+        Enumeration<Rental> rentals = _rentals.elements();
+        String result = "Rental Record for " + getName() + "\n";
+        while (rentals.hasMoreElements()) {
+            Rental each = rentals.nextElement();
+            double thisAmount = each.getCharge(); // Atualizada a chamada para o método getCharge
 
-         // Adiciona pontos de locação frequente
-         frequentRenterPoints++;
-         if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) 
+            // Adiciona pontos de locação frequente
             frequentRenterPoints++;
+            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) && each.getDaysRented() > 1) {
+                frequentRenterPoints++;
+            }
 
-         // Mostra os valores desta locação
-         result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-         totalAmount += thisAmount;
-      }
-      // Adiciona linhas finais
-      result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-      result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-      return result;
-   }
-
-   private double amountFor(Rental aRental) {
-      double thisAmount = 0;
-      switch (aRental.getMovie().getPriceCode()) {
-         case Movie.REGULAR:
-            thisAmount += 2;
-            if (aRental.getDaysRented() > 2) 
-               thisAmount += (aRental.getDaysRented() - 2) * 1.5;
-            break;
-         case Movie.NEW_RELEASE:
-            thisAmount += aRental.getDaysRented() * 3;
-            break;
-         case Movie.CHILDRENS:
-            thisAmount += 1.5;
-            if (aRental.getDaysRented() > 3) 
-               thisAmount += (aRental.getDaysRented() - 3) * 1.5;
-            break;
-      }
-      return thisAmount;
-   }
+            // Mostra os valores desta locação
+            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
+            totalAmount += thisAmount;
+        }
+        // Adiciona linhas finais
+        result += "Amount owed is " + totalAmount + "\n";
+        result += "You earned " + frequentRenterPoints + " frequent renter points";
+        return result;
+    }
 }
